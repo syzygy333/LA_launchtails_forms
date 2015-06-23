d77ef1cd8d3cc1923a41be7a1098d97f119db038
syzygy333 <brianjonashogue@gmail.com>
2015-06-23 09:06:10 -0400

    Initial commit

---
+++ app/controllers/drinks_controller.rb
@@ -0,0 +1,29 @@
+class DrinksController < ApplicationController
+  def new
+    @drink = Drink.new
+  end
+
+  def create
+    @drink = Drink.new(drink_params)
+    if @drink.save
+      flash[:notice] = 'Drink added.'
+      redirect_to '/drinks'
+    else
+      render :new
+    end
+  end
+
+  def index
+    if params[:category_id] != nil
+      @category = Category.find(params[:category_id])
+      @drinks = @category.drinks
+    else
+      @drinks = Drink.all
+    end
+  end
+
+  protected
+  def drink_params
+    params.require(:drink).permit(:title, :description)
+  end
+end
